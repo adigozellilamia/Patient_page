0c1d0dad73e4abc2ad91640cf4e04516f368da91
= <=>
2018-06-30 01:43:16 +0400

    patientPage

--- js/script.js
+++ js/script.js
@@ -4,11 +4,14 @@ var button_icon = document.querySelectorAll(".btn-menu span");
 var patient = document.querySelector(".patient");
 var content =  document.querySelector(".content");
 
+var body = document.querySelector("body");
+var patient_responsive =  document.querySelector(".patient-responsive");
 button.addEventListener("click", function () {
     console.log("click olundu")
-    
+
     if(patient.style.display == "none"){
-       
+        patient_responsive.style.height="100vh";
+        body.style.overflowY="hidden"
         content.style.display = "none";
         patient.style.display = "block";
         button_icon[0].style.transform = "rotateZ(40deg)";
@@ -16,7 +19,6 @@ button.addEventListener("click", function () {
         button_icon[0].style.transitions = "0.4s ease";
 
         button_icon[1].style.transform = "translate(-100px) rotateZ(0deg)";
-        // button_icon[1].style.top = "23px";
         button_icon[1].style.opacity = "0";
         button_icon[1].style.transitions = "0.4s ease";
 
@@ -27,6 +29,8 @@ button.addEventListener("click", function () {
         
     }
     else{
+        patient_responsive.style.height="100%";
+        body.style.overflowY="auto"
         console.log("block")
         patient.style.display = "none";
         content.style.display = "block"
